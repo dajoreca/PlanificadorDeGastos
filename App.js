f9d25c4abd0bde32234e22f6456fd2d4b1abe823
Daniel Reollo <dreollo92@gmail.com>
2022-05-02 18:33:46 +0200

    ProjectExpo

--- App.js
+++ App.js
@@ -1,20 +1,137 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+import React, {useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+  Alert,
+  Pressable,
+  Image,
+  Modal
+
+} from 'react-native';
+//import deepDiffer from 'react-native/Libraries/Utilities/differ/deepDiffer';
+
+import Header from './src/components/Header';
+import NuevoPresupuesto from './src/components/NuevoPresupuesto';
+import ControlPresupuesto from './src/components/ControlPresupuesto';
+import FormularioGasto from './src/components/FormularioGasto'
+import { generarId } from './src/Helpers'
+
+
+const App = () => {
+
+  const [isValidPresupuesto,setIsValidPresupuesto] = useState(false)
+  const [presupuesto,setPresupuesto] = useState(0)
+  const [gastos, setGastos] = useState([])
+  const [modal, setModal] = useState(false)
+  
+  const handleNuevoPresupuesto = (presupuesto) => {
+    if (Number(presupuesto) > 0){
+      setIsValidPresupuesto(true)
+    } else{
+        Alert.alert('Error', 'El presupuesto no puede ser 0 o menor', [{ text: "OK" }])
+    }
+  }
+
+
+  const handleGasto = gasto => {
+
+      if(Object.values(gasto).includes('')) {
+        Alert.alert(
+          "Error",
+          "Todos los campos son obligatorios",
+          [{text: 'OK'}]
+        )
+        return
+      }
+
+      //Añadir el nuevo gasto al state
+      gasto.id = generarId()
+
+      console.log(gastos)
+      console.log("Nuevo Gasto", gasto)
+
+      setGastos([...gastos, gasto])
+      setModal(!modal)
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.contenedor}>
+      <View style={styles.header}>
+          <Header />
+
+          {isValidPresupuesto ? (
+            <ControlPresupuesto
+              presupuesto={presupuesto} 
+              gastos={gastos}
+            /> 
+          ) : (
+          
+            <NuevoPresupuesto 
+              presupuesto={presupuesto}
+              setPresupuesto={setPresupuesto}
+              handleNuevoPresupuesto={handleNuevoPresupuesto}
+          />
+          
+          )}
+          
+      </View>
+      
+          {modal && (
+            <Modal
+              animationType='slide'
+              visible={modal}
+              onRequestClose={() =>{
+                setModal(!modal)
+              }}
+
+            >
+              <FormularioGasto 
+                setModal={setModal}
+                handleGasto={handleGasto}
+              />
+
+            </Modal>
+          )}
+
+      {isValidPresupuesto && (
+        <Pressable
+          onPress={() => setModal(!modal)}
+        
+        >
+          <Image 
+            style={styles.imagen}
+            source={require('./src/img/nuevo-gasto.png')}
+          />
+        </Pressable>
+
+      )}
+    </View>     
   );
-}
+};
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  contenedor:{
+    backgroundColor: '#F5F5F5',
+    flex: 1
   },
+  header: {
+    backgroundColor:'#3B82F6',
+
+  },
+  imagen:{
+    width: 60,
+    height: 60,
+    position: 'absolute',
+    top: 55,
+    right: 10
+
+  }
+
 });
+
+export default App;
---
+++ src/components/ControlPresupuesto.js
@@ -0,0 +1,84 @@
+
+import {Text, View, Image, StyleSheet} from  'react-native'
+import React, {useState, useEffect} from 'react'
+import globalStyles from '../Styles'
+import { formatearCantidad } from '../Helpers'
+
+const ControlPresupuesto = ({presupuesto, gastos}) => {
+    const [disponible, setDisponible] = useState(0)
+    const [gastado, setGastado] = useState(0)
+
+    useEffect(() => {
+        const totalGastado = gastos.reduce( (total, gasto) => Number(gasto.cantidad ) + total, 0 )
+        const totalDisponible = presupuesto - totalGastado
+
+        setGastado(totalGastado)
+        setDisponible(totalDisponible)
+    }, [])
+
+  return (
+    <View style={styles.contenedor}>
+        <View style={styles.centrarGrafica}>
+            <Image
+                style={styles.imagen} 
+                source={ require('../img/grafico.jpg')}/>
+
+        </View>
+        <View style={styles.contenedorTexto}>
+            <Text style={styles.valor}>
+                <Text style={styles.label}>Presupuesto: € </Text>
+
+                    {formatearCantidad(presupuesto)}
+            </Text>
+
+            <Text style={styles.valor}>
+                <Text style={styles.label}>Disponible: € </Text>
+
+                    {formatearCantidad(disponible)}
+            </Text>
+
+            <Text style={styles.valor}>
+                <Text style={styles.label}>Gastado: € </Text>
+
+                    {formatearCantidad(gastado)}
+            </Text>
+
+        </View>
+
+    </View>
+    
+    )
+}
+
+const styles = StyleSheet.create({
+    contenedor:{
+       ...globalStyles.contenedor
+    },
+    centrarGrafica:{
+        alignItems:'center'
+
+    },
+    imagen:{
+        width: 250,
+        height: 250
+
+    },
+    contenedorTexto:{
+        marginTop: 40
+
+    },
+    valor:{
+        fontSize: 25,
+        textAlign: 'center',
+        marginBottom: 2
+    },
+    label:{
+         fontWeight: '700',
+         color: '#3B82F6'
+
+    },
+
+
+})
+
+export default ControlPresupuesto
